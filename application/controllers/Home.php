76e8b832b994b3333e93e642911e28a139e3a229
BootBuilder <rotyjeremy@gmail.com>
2015-12-16 17:27:43 +0100

    Social Networking Lib Google/Facebook/Twitter/Etc

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -5,6 +5,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Home extends CI_Controller {
 
     public function index() {
+        redirect('Auth/login');
         $data['title'] = "Welcome to CodeigniterAdvanced";
         $this->Core->load("page/home", $data);
     }
