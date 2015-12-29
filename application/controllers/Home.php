dc69dbc6facfb170d7f808e1daeb86d87f265e0a
BootBuilder <rotyjeremy@gmail.com>
2015-12-29 19:05:42 +0100

    DELETE SOCIAL LINK

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -5,7 +5,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Home extends CI_Controller {
 
     public function index() {
-        redirect('Auth/login');
         $data['title'] = "Welcome to CodeigniterAdvanced";
         $this->Core->load("page/home", $data);
     }
